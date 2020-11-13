71d2411a7f65f6fee865cbc657521e0f4f29ed48
alexandr <karpov.a1exandr@yandex.ru>
2020-11-13 15:59:15 +0300

    убрал лишние строки

--- marvel_comics/urls.py
+++ marvel_comics/urls.py
@@ -18,13 +18,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework.routers import SimpleRouter
 
-# from marvel.views import ComicsView
-#
-# router = SimpleRouter()
-#
-# router.register('http://gateway.marvel.com/v1/public/comics', ComicsView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -36,4 +30,3 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# urlpatterns += router.urls
